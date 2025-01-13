1e69065f51aad36730b307aad45f127089b83a75
soray677 <soray677@outlook.com>
2025-01-13 23:36:53 +0900

    ポートフォリオの文字を見やすく

--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -3,7 +3,7 @@ import TypingText from "../components/TypingText";
 export default function Index() {
   return (
     <>
-      <div className="absolute flex justify-center w-full top-0 mt-4 mx-auto text-[clamp(2rem,7vw,3rem)]">
+      <div className="absolute flex justify-center w-full top-0 mt-4 mx-auto text-[clamp(2rem,7vw,3rem)] mix-blend-difference	">
         <TypingText text={"Sora's Portfolio"} />
       </div>
     </>
