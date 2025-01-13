874c515fa3df1a525e69d6c2abda2e219d31b22a
soray677 <soray677@outlook.com>
2025-01-13 23:39:09 +0900

    クローズボタンを追加

--- portfolio/src/layouts/ContentLayout.tsx
+++ portfolio/src/layouts/ContentLayout.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import MainTitle from "../components/MainTitle";
 
 type Props = {
@@ -8,6 +9,9 @@ type Props = {
 const ContentLayout = ({ title, children }: Props) => {
   return (
     <div className="absolute top-0 z-100 h-full w-full max-w-full max-h-full overflow-y-scroll p-2">
+      <div className="flex justify-end">
+        <Link href="/">x Close</Link>
+      </div>
       <section className="md:w-[80%] w-[95%] min-h-[90%] max-w-[640px] overflow-hidden  mx-auto text-black bg-white bg-opacity-80 rounded-md p-4 m-3 break-words">
         <div className="flex w-full mb-10 justify-center">
           <MainTitle text={title} />
