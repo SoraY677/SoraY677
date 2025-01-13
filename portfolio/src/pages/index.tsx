47ea1459a0a6f3d2b78a7ed1d22a91fffbc27af6
soray677 <soray677@outlook.com>
2025-01-13 22:57:48 +0900

    menuを共通パーツへ移したため削除

--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -1,20 +1,13 @@
-import { useEffect, useState } from "react";
 import Menu from "../components/Menu";
 import TypingText from "../components/TypingText";
+import { routeList } from "../config/MenuConfig";
 
 export default function Index() {
-  const [routes, setRoutes] = useState<Routes>([]);
-  useEffect(() => {
-    setRoutes([{ text: "about", path: "/about" }]);
-  }, []);
   return (
     <>
-      <div className="absolute flex justify-center w-full top-0 mt-4 mx-auto text-[7vw]">
+      <div className="absolute flex justify-center w-full top-0 mt-4 mx-auto text-[clamp(2rem,7vw,3rem)]">
         <TypingText text={"Sora's Portfolio"} />
       </div>
-      <nav className="absolute right-0 bottom-0 text-[2rem]">
-        <Menu routes={routes} />
-      </nav>
     </>
   );
 }
