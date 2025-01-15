ba35e7702c22c758161980fbaaf216486fa23e3f
soray677 <soray677@outlook.com>
2025-01-16 00:57:46 +0900

    CLOSEボタン調整

--- portfolio/src/layouts/ContentLayout.tsx
+++ portfolio/src/layouts/ContentLayout.tsx
@@ -9,14 +9,18 @@ type Props = {
 const ContentLayout = ({ title, children }: Props) => {
   return (
     <div className="absolute top-0 z-100 h-full w-full max-w-full max-h-full overflow-y-scroll px-2 pb-[150px] md:pb-[250px]">
-      <div className="flex justify-end py-1 pr-1">
-        <Link href="/">x Close</Link>
-      </div>
-      <section className="md:w-[80%] w-[95%] min-h-[90%] max-w-[640px] overflow-hidden mx-auto text-secondary bg-primary bg-opacity-80 rounded-md p-4 m-3 break-words">
-        <div className="flex w-full mb-10 justify-center">
-          <MainTitle text={title} />
+      <section className="md:w-[80%] w-[95%] min-h-[90%] max-w-[640px] overflow-hidden mx-auto  bg-opacity-80 rounded-md m-3 break-words">
+        <div className="flex justify-end p-1">
+          <Link href="/" className="text-md md:text-lg mix-blend-difference">
+            X CLOSE
+          </Link>
         </div>
-        {children}
+        <div className="p-4  text-secondary bg-primary">
+          <div className="flex w-full mb-10 justify-center">
+            <MainTitle text={title} />
+          </div>
+          {children}
+        </div>
       </section>
     </div>
   );
