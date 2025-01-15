598588a559cae07a3331d85210f5d0e30175bde2
soray677 <soray677@outlook.com>
2025-01-15 13:28:43 +0900

    menu -> menus

--- portfolio/src/config/MenuConfig.ts
+++ portfolio/src/config/MenuConfig.ts
@@ -4,7 +4,7 @@ import ProfileIcon from "../assets/ProfileIcon";
 
 export const routeList = [
   { text: "about", path: "/about", icon: ProfileIcon },
-  { text: "works", path: "/work", icon: IdeaIcon },
+  { text: "works", path: "/works", icon: IdeaIcon },
   {
     text: "form",
     path: "https://docs.google.com/forms/d/e/1FAIpQLSd8H3BFMBC8cLKJKE2K1szrEUZqAnmQ8PdYCT911OA0JLRbVQ/viewform",
