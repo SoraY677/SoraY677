8df88b2a651e73927805fb9e2fe0bd9e2a8c3e06
soray677 <soray677@outlook.com>
2025-01-15 23:31:23 +0900

    URLを修正

--- portfolio/src/config/MetaConfig.ts
+++ portfolio/src/config/MetaConfig.ts
@@ -3,6 +3,6 @@ export const META_DESCRIPTION =
   "こんにちは!Soraと申します。形式や固定概念にとらわれずITに関するお悩みあれこれの解決に挑んでます。悩み事はお気軽にご相談ください!";
 export const META_TWITTER_CARD_TYPE = "summary";
 export const META_APP_ICON_PATH = "/app-icon.png";
-export const META_APP_ICON_URL = `${
-  process.env.NEXT_PUBLIC_VERCEL_URL ?? "http://localhost:3000"
-}${META_APP_ICON_PATH}`;
+export const META_APP_ICON_URL = `${`https://${
+  process.env.NEXT_PUBLIC_VERCEL_URL ?? "localhost:3000"
+}`}${META_APP_ICON_PATH}`;
