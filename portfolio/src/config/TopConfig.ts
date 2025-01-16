78f67d1be41495adc9ed53794decbddee2773ec2
soray677 <soray677@outlook.com>
2025-01-16 15:39:43 +0900

    サブタイトル追加

--- portfolio/src/config/TopConfig.ts
+++ portfolio/src/config/TopConfig.ts
@@ -1,3 +1,4 @@
+export const PORTFOLIO_SUB_TITLE = "GENERAL-ENGENEER";
 export const PORTFOLIO_TITLE = "Sora's Portfolio";
 
 export const TYPING_SPEED_MS = 100;
--- portfolio/src/pages/index.tsx
+++ portfolio/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import TypingText from "../components/TypingText";
-import { PORTFOLIO_TITLE } from "../config/TopConfig";
+import { PORTFOLIO_SUB_TITLE, PORTFOLIO_TITLE } from "../config/TopConfig";
 import PageTransitionLayout from "../layouts/PageTransitionLayout";
 import { META_TITLE_BASE } from "../config/MetaConfig";
 
@@ -10,9 +10,16 @@ export default function Index() {
       <Head>
         <title>{`${META_TITLE_BASE} | TOP`}</title>
       </Head>
-      <div className="absolute flex justify-center w-full top-0 mt-4 mx-auto text-[clamp(2rem,7vw,3rem)] mix-blend-difference">
+      <div className="absolute flex justify-center w-full top-0 mt-4 mx-auto  mix-blend-difference">
         <PageTransitionLayout>
-          <TypingText text={PORTFOLIO_TITLE} />
+          <>
+            <p className="text-center text-[clamp-0.8rem,4vw,1.4rem)]">
+              {PORTFOLIO_SUB_TITLE}
+            </p>
+            <p className="text-[clamp(2rem,7vw,3rem)]">
+              <TypingText text={PORTFOLIO_TITLE} />
+            </p>
+          </>
         </PageTransitionLayout>
       </div>
     </>
